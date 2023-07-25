fe13d5d01c5049875ac79537716ae83de810aa95
Momen Qudaih <momenqudaih1@gmail.com>
2023-07-25 11:42:09 +0300

    Apply Redux Thunk Middleware

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -114,7 +114,6 @@ export const selectAllPosts = (state) => state.posts.posts;
 export const selectPostsStatus = (state) => state.posts.status;
 export const selectPostsError = (state) => state.posts.error;
 
-
 export const { postAdded, reactionAdded } = postsSlice.actions;
 
 export default postsSlice.reducer;
